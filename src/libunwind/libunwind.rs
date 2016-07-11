83985bcf74fc10798c893a7ecd62e5611c2d3257
Samuel Moritz <samuel.moritz@gmail.com>
2016-07-11 22:41:02 +0200

    Building a hello world-app without jemalloc now works if linking manually on the target system. But running the app crashes.

--- src/libunwind/libunwind.rs
+++ src/libunwind/libunwind.rs
@@ -102,6 +102,7 @@ pub type _Unwind_Exception_Cleanup_Fn = extern "C" fn(unwind_code: _Unwind_Reaso
 
 #[cfg_attr(any(all(target_os = "linux", not(target_env = "musl")),
                target_os = "freebsd",
+               target_os = "kfreebsd",
                target_os = "solaris",
                all(target_os = "linux",
                    target_env = "musl",
