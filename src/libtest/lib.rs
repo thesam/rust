1af188c53a1c6c58118060b7c7338d52d0722890
Samuel Moritz <samuel.moritz@gmail.com>
2016-07-11 13:33:52 +0200

    make rustc-stage1 now works!

--- src/libtest/lib.rs
+++ src/libtest/lib.rs
@@ -955,6 +955,7 @@ fn get_concurrency() -> usize {
     }
 
     #[cfg(any(target_os = "freebsd",
+              target_os = "kfreebsd",
               target_os = "dragonfly",
               target_os = "bitrig",
               target_os = "netbsd"))]
