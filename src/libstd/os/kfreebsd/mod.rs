9ee0e072a14809741cf0bb7d0e8a962693a8ae10
Samuel Moritz <samuel.moritz@gmail.com>
2016-08-05 20:16:22 +0200

    kfreebsd-new

--- src/libstd/os/kfreebsd/mod.rs
+++ src/libstd/os/kfreebsd/mod.rs
@@ -8,7 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-//! Linux-specific definitions
+//! FreeBSD-specific definitions
 
 #![stable(feature = "raw_ext", since = "1.1.0")]
 
